7bb5d196ad4b16af756009ddb794441092f7d320
Michael Rose <michael@rosenetwork.net>
2023-09-21 21:06:11 -0700

    section headings now have an id eq to title

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -8,7 +8,7 @@ interface separatorData {
 
 const SeparatorDemo = ({title,subtitle}: separatorData) => (
   <div className="mt-6 mx-auto sticky top-0 z-10 w-full sectionHeading relative">
-    <h1 className="dark:text-white mt-4 text-black text-sm font-medium absolute left-0 right- 0 mb-14">
+    <h1 className="dark:text-white mt-4 text-black text-sm font-medium absolute left-0 right- 0 mb-14" id={title}>
       {title}
     </h1>
     <h2 className="dark:text-white  mt-4 text-grey-500 text-xl">
