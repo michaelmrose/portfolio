2c428b7f7cb3e11ad742e820d29fbd2fcba29f5e
Michael Rose <michael@rosenetwork.net>
2023-09-22 00:41:10 -0700

    more appropriate naming and formatting

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import * as Separator from "@radix-ui/react-separator";
 
-interface separatorData {
+interface SectionHeaderData {
   title: string
   subtitle: string
 }
 
-const SeparatorDemo = ({ title, subtitle }: separatorData) => (
-  <div className="mt-6 mx-auto sticky top-0 z-10 w-full sectionHeading relative">
-    <h1 className="dark:text-white mt-4 text-black text-sm font-medium absolute left-0 right- 0 mb-14" id={title}>
-      {title}
-    </h1>
-    <h2 className="dark:text-white  mt-4 text-grey-500 text-xl">
-      {subtitle}
-    </h2>
-  </div>
+export default function SectionHeader({ title, subtitle }: SectionHeaderData) {
+  return (
+    <div className="mt-6 mx-auto sticky top-0 z-10 w-full sectionHeading relative">
+      <h1 className="dark:text-white mt-4 text-black text-sm font-medium absolute left-0 right- 0 mb-14" id={title}>
+        {title}
+      </h1>
+      <h2 className="dark:text-white  mt-4 text-grey-500 text-xl">
+        {subtitle}
+      </h2>
+    </div>
+  )
 
-);
+}
 
-export default SeparatorDemo;
